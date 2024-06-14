43c9d6cbd2ae8fd48129b1896126ae5abd6da6d3
MacDuki <PCMYACT@hotmail.com>
2024-06-14 15:29:49 -0300

    preview function complete

--- js/obligatorio.js
+++ js/obligatorio.js
@@ -542,7 +542,8 @@ function renderHistoryOfPurchasesHTML() {
 	<div>
 	<h2>Historial de compra</h2>
 	<div class="historyOfPurchasesContainer"></div>
-	</div>		
+	</div>
+	
 	`;
 	HTMLSECTION.innerHTML = renderSection;
 	sistema.listaCompras.forEach((compra) => {
@@ -771,14 +772,65 @@ function renderAddNewProductAdmin() {
 
         <button class="btnForm">Crear Producto</button>
     </div>
+		<div id="productPreview"></div>	
     `;
 	HTMLSECTION.innerHTML = renderSection;
+	document.querySelectorAll("#createProductForm input").forEach((input) => {
+		input.addEventListener("input", handleUpdateProductPreview);
+	});
 
 	addEventListenerSafely(".btnForm", "click", handleAddNewProductAdmin);
 }
 
 /*HANDLES --------------------------------------*/
 
+function handleUpdateProductPreview() {
+	const name =
+		document.getElementById("txtName").value || "Nombre del producto";
+	const price = document.getElementById("productValue").value || 0;
+	const description =
+		document.getElementById("productDescription").value ||
+		"Descripción del producto";
+	const urlImage =
+		document.getElementById("urlImage").value ||
+		"https://camarasal.com/wp-content/uploads/2020/08/default-image-5-1.jpg";
+	const stock = document.getElementById("productStock").value || 0;
+	const rating = 5;
+
+	const renderSectionProductPreview = /*html*/ `
+	<h3>Preview</h3>
+      <div class="productoDetailsContainer  productoDetailsContainerPreview ${
+				isNightMode ? "nightMode" : ""
+			}">
+        <h2 class="titleProductoDetails">${name}</h2>
+        <div class="productoDetailsContainerImg">
+          <img src="${urlImage}" alt="imagenRandome" />
+        </div>
+        <div class='spansContainerProductoDetails'>
+          <span>${description}</span>    
+          <div class="priceAndStockContainer">
+            <span class="priceProductoDetails">$ ${price}</span>
+            <span class="stockProductoDetails">Stock: ${stock}</span>
+          </div>
+          <span class="ratingProductoDetails">${handleRating(rating)}</span>
+        </div>
+        <div class="productoDetailsControlsContainer">
+          <label>Cuantas unidades?<input type="number" min="1" id="btnSelectStockBuyer" placeholder="1" value="selectStock" /></label>
+          <button class="CartBtn">
+      <span class="IconContainer"> 
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
+      </span>
+      <p class="text">Add to Cart</p>
+    </button>
+        </div>
+      </div>
+    `;
+
+	const productPreviewElement = document.getElementById("productPreview");
+	if (productPreviewElement) {
+		productPreviewElement.innerHTML = renderSectionProductPreview;
+	}
+}
 function handleProductsView() {
 	if (isInSales) {
 		isInSales = false;
